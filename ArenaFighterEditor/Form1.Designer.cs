2c14d3ee3482fc84f10b515d70422c0c643a998a
Thomas Sapp <tpsapp@hotmail.com>
2020-11-12 18:40:11 -0700

    Add project files.

---
+++ ArenaFighterEditor/Form1.Designer.cs
@@ -0,0 +1,406 @@
+﻿namespace ArenaFighterEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.playerSex = new System.Windows.Forms.TextBox();
+            this.playerLevel = new System.Windows.Forms.TextBox();
+            this.playerExp = new System.Windows.Forms.TextBox();
+            this.playerPotions = new System.Windows.Forms.TextBox();
+            this.playerAgility = new System.Windows.Forms.TextBox();
+            this.playerStrength = new System.Windows.Forms.TextBox();
+            this.playerMaxHealth = new System.Windows.Forms.TextBox();
+            this.playerGold = new System.Windows.Forms.TextBox();
+            this.playerName = new System.Windows.Forms.TextBox();
+            this.label23 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.loadFile = new System.Windows.Forms.Button();
+            this.saveFile = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.playerHealth = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.playerWeapon = new System.Windows.Forms.ComboBox();
+            this.playerArmor = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // playerSex
+            // 
+            this.playerSex.BackColor = System.Drawing.Color.White;
+            this.playerSex.Location = new System.Drawing.Point(64, 55);
+            this.playerSex.Name = "playerSex";
+            this.playerSex.Size = new System.Drawing.Size(100, 20);
+            this.playerSex.TabIndex = 4;
+            // 
+            // playerLevel
+            // 
+            this.playerLevel.BackColor = System.Drawing.Color.White;
+            this.playerLevel.Location = new System.Drawing.Point(64, 81);
+            this.playerLevel.Name = "playerLevel";
+            this.playerLevel.Size = new System.Drawing.Size(100, 20);
+            this.playerLevel.TabIndex = 5;
+            // 
+            // playerExp
+            // 
+            this.playerExp.BackColor = System.Drawing.Color.White;
+            this.playerExp.Location = new System.Drawing.Point(64, 107);
+            this.playerExp.Name = "playerExp";
+            this.playerExp.Size = new System.Drawing.Size(100, 20);
+            this.playerExp.TabIndex = 6;
+            // 
+            // playerPotions
+            // 
+            this.playerPotions.BackColor = System.Drawing.Color.White;
+            this.playerPotions.Location = new System.Drawing.Point(64, 291);
+            this.playerPotions.Name = "playerPotions";
+            this.playerPotions.Size = new System.Drawing.Size(100, 20);
+            this.playerPotions.TabIndex = 14;
+            // 
+            // playerAgility
+            // 
+            this.playerAgility.BackColor = System.Drawing.Color.White;
+            this.playerAgility.Location = new System.Drawing.Point(64, 211);
+            this.playerAgility.Name = "playerAgility";
+            this.playerAgility.Size = new System.Drawing.Size(100, 20);
+            this.playerAgility.TabIndex = 11;
+            // 
+            // playerStrength
+            // 
+            this.playerStrength.BackColor = System.Drawing.Color.White;
+            this.playerStrength.Location = new System.Drawing.Point(64, 185);
+            this.playerStrength.Name = "playerStrength";
+            this.playerStrength.Size = new System.Drawing.Size(100, 20);
+            this.playerStrength.TabIndex = 10;
+            // 
+            // playerMaxHealth
+            // 
+            this.playerMaxHealth.BackColor = System.Drawing.Color.White;
+            this.playerMaxHealth.Location = new System.Drawing.Point(170, 159);
+            this.playerMaxHealth.Name = "playerMaxHealth";
+            this.playerMaxHealth.Size = new System.Drawing.Size(100, 20);
+            this.playerMaxHealth.TabIndex = 9;
+            // 
+            // playerGold
+            // 
+            this.playerGold.BackColor = System.Drawing.Color.White;
+            this.playerGold.Location = new System.Drawing.Point(64, 133);
+            this.playerGold.Name = "playerGold";
+            this.playerGold.Size = new System.Drawing.Size(100, 20);
+            this.playerGold.TabIndex = 7;
+            // 
+            // playerName
+            // 
+            this.playerName.BackColor = System.Drawing.Color.White;
+            this.playerName.Location = new System.Drawing.Point(64, 29);
+            this.playerName.Name = "playerName";
+            this.playerName.Size = new System.Drawing.Size(206, 20);
+            this.playerName.TabIndex = 3;
+            // 
+            // label23
+            // 
+            this.label23.AutoSize = true;
+            this.label23.Location = new System.Drawing.Point(21, 214);
+            this.label23.Name = "label23";
+            this.label23.Size = new System.Drawing.Size(37, 13);
+            this.label23.TabIndex = 35;
+            this.label23.Text = "Agility:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(13, 292);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(45, 13);
+            this.label11.TabIndex = 34;
+            this.label11.Text = "Potions:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(21, 266);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(37, 13);
+            this.label10.TabIndex = 33;
+            this.label10.Text = "Armor:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(7, 240);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(51, 13);
+            this.label9.TabIndex = 32;
+            this.label9.Text = "Weapon:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(8, 188);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(50, 13);
+            this.label8.TabIndex = 31;
+            this.label8.Text = "Strength:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(17, 162);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 13);
+            this.label7.TabIndex = 30;
+            this.label7.Text = "Health:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(26, 136);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 13);
+            this.label6.TabIndex = 29;
+            this.label6.Text = "Gold:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 110);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 13);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "Exp:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "Level:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Sex:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Name:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Player Information";
+            // 
+            // loadFile
+            // 
+            this.loadFile.Location = new System.Drawing.Point(15, 324);
+            this.loadFile.Name = "loadFile";
+            this.loadFile.Size = new System.Drawing.Size(75, 23);
+            this.loadFile.TabIndex = 1;
+            this.loadFile.Text = "&Load File";
+            this.loadFile.UseVisualStyleBackColor = true;
+            this.loadFile.Click += new System.EventHandler(this.loadFile_Click);
+            // 
+            // saveFile
+            // 
+            this.saveFile.Location = new System.Drawing.Point(96, 324);
+            this.saveFile.Name = "saveFile";
+            this.saveFile.Size = new System.Drawing.Size(75, 23);
+            this.saveFile.TabIndex = 2;
+            this.saveFile.Text = "&Save File";
+            this.saveFile.UseVisualStyleBackColor = true;
+            this.saveFile.Click += new System.EventHandler(this.saveFile_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(177, 324);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 15;
+            this.exitButton.Text = "E&xit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // playerHealth
+            // 
+            this.playerHealth.BackColor = System.Drawing.Color.White;
+            this.playerHealth.Location = new System.Drawing.Point(64, 159);
+            this.playerHealth.Name = "playerHealth";
+            this.playerHealth.Size = new System.Drawing.Size(100, 20);
+            this.playerHealth.TabIndex = 8;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "dat";
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Arena Fighter Files|*.dat";
+            // 
+            // playerWeapon
+            // 
+            this.playerWeapon.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.playerWeapon.FormattingEnabled = true;
+            this.playerWeapon.Items.AddRange(new object[] {
+            "Dagger",
+            "Club",
+            "Staff",
+            "Sword",
+            "Mace",
+            "Steel Sword",
+            "Steel Mace",
+            "Silver Sword",
+            "Silver Mace",
+            "Gold Sword",
+            "Gold Mace"});
+            this.playerWeapon.Location = new System.Drawing.Point(64, 237);
+            this.playerWeapon.Name = "playerWeapon";
+            this.playerWeapon.Size = new System.Drawing.Size(100, 21);
+            this.playerWeapon.TabIndex = 36;
+            // 
+            // playerArmor
+            // 
+            this.playerArmor.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.playerArmor.FormattingEnabled = true;
+            this.playerArmor.Items.AddRange(new object[] {
+            "Cloth",
+            "Leather",
+            "Chain",
+            "Plate",
+            "Scale",
+            "Steel Chain",
+            "Steel Plate",
+            "Steel Scale",
+            "Titanium Chain",
+            "Titanium Plate",
+            "Titanium Scale"});
+            this.playerArmor.Location = new System.Drawing.Point(64, 264);
+            this.playerArmor.Name = "playerArmor";
+            this.playerArmor.Size = new System.Drawing.Size(100, 21);
+            this.playerArmor.TabIndex = 37;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(280, 349);
+            this.Controls.Add(this.playerArmor);
+            this.Controls.Add(this.playerWeapon);
+            this.Controls.Add(this.playerHealth);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.saveFile);
+            this.Controls.Add(this.loadFile);
+            this.Controls.Add(this.playerSex);
+            this.Controls.Add(this.playerLevel);
+            this.Controls.Add(this.playerExp);
+            this.Controls.Add(this.playerPotions);
+            this.Controls.Add(this.playerAgility);
+            this.Controls.Add(this.playerStrength);
+            this.Controls.Add(this.playerMaxHealth);
+            this.Controls.Add(this.playerGold);
+            this.Controls.Add(this.playerName);
+            this.Controls.Add(this.label23);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Arena Fighter Editor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.TextBox playerSex;
+        public System.Windows.Forms.TextBox playerLevel;
+        public System.Windows.Forms.TextBox playerExp;
+        public System.Windows.Forms.TextBox playerPotions;
+        public System.Windows.Forms.TextBox playerAgility;
+        public System.Windows.Forms.TextBox playerStrength;
+        public System.Windows.Forms.TextBox playerMaxHealth;
+        public System.Windows.Forms.TextBox playerGold;
+        public System.Windows.Forms.TextBox playerName;
+        private System.Windows.Forms.Label label23;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button loadFile;
+        private System.Windows.Forms.Button saveFile;
+        private System.Windows.Forms.Button exitButton;
+        public System.Windows.Forms.TextBox playerHealth;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ComboBox playerWeapon;
+        private System.Windows.Forms.ComboBox playerArmor;
+    }
+}
+
---
+++ ArenaFighterEditor/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ArenaFighterEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void loadFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                TextReader characterFile = new StreamReader(openFileDialog1.FileName);
+                int weapon, armor;
+                playerName.Text = characterFile.ReadLine();
+                playerSex.Text = characterFile.ReadLine();
+                playerLevel.Text = characterFile.ReadLine();
+                playerExp.Text = characterFile.ReadLine();
+                playerGold.Text = characterFile.ReadLine();
+                playerHealth.Text = characterFile.ReadLine();
+                playerMaxHealth.Text = characterFile.ReadLine();
+                playerStrength.Text = characterFile.ReadLine();
+                playerAgility.Text = characterFile.ReadLine();
+                weapon = Convert.ToInt16(characterFile.ReadLine()) - 1;
+                armor = Convert.ToInt16(characterFile.ReadLine()) - 1;
+                playerPotions.Text = characterFile.ReadLine();
+                characterFile.Close();
+                if ((weapon > 11) || (weapon < 0))
+                {
+                    playerWeapon.SelectedIndex = 0;
+                }
+                else
+                {
+                    playerWeapon.SelectedIndex = weapon;
+                }
+                if ((armor > 11) || (armor < 0))
+                {
+                    playerArmor.SelectedIndex = 0;
+                }
+                else
+                {
+                    playerArmor.SelectedIndex = armor;
+                }
+            }
+        }
+
+        private void saveFile_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.FileName = playerName.Text + ".dat";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                TextWriter characterFile = new StreamWriter(saveFileDialog1.FileName);
+                characterFile.WriteLine(playerName.Text);
+                characterFile.WriteLine(playerSex.Text);
+                characterFile.WriteLine(playerLevel.Text);
+                characterFile.WriteLine(playerExp.Text);
+                characterFile.WriteLine(playerGold.Text);
+                characterFile.WriteLine(playerHealth.Text);
+                characterFile.WriteLine(playerMaxHealth.Text);
+                characterFile.WriteLine(playerStrength.Text);
+                characterFile.WriteLine(playerAgility.Text);
+                characterFile.WriteLine(playerWeapon.SelectedIndex + 1);
+                characterFile.WriteLine(playerArmor.SelectedIndex + 1);
+                characterFile.WriteLine(playerPotions.Text);
+                characterFile.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = Environment.CurrentDirectory;
+            saveFileDialog1.InitialDirectory = Environment.CurrentDirectory;
+        }
+    }
+}
